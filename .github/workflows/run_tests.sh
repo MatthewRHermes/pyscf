b22b892c2a8e711f79d549e20eed0e7354f424d9
Matthew R Hermes <mrhermes@uchicago.edu>
2023-07-04 09:51:47 -0500

    Run single test only

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -7,5 +7,5 @@ echo 'pbc_tools_pbc_fft_engine = "NUMPY"' > .pyscf_conf.py
 echo "dftd3_DFTD3PATH = './pyscf/lib/deps/lib'" >> .pyscf_conf.py
 echo "scf_hf_SCF_mute_chkfile = True" >> .pyscf_conf.py
 
-pytest pyscf/ -s -c setup.cfg \
+pytest pyscf/ci/test/test_gcisd.py -s -c setup.cfg \
     --cov-report xml --cov-report term --cov-config .coveragerc --cov pyscf
